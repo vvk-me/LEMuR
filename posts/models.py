3813e984fde69cee6b7415e60361f40498c77e9e
VVK.me <krivorotvv@gmail.com>
2015-10-23 03:08:19 +1100

    Post models 1

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
+    title = models.CharField(max_length = 50, blank = True)
     body = models.TextField()
+    user = models.ForeignKey(User, blank = True)
+    ref = models.ForeignKey('self', blank = True)
+    created = models.DateTimeField(auto_now_add = True)
+    modified = models.DateTimeField(auto_now = True)
+    
